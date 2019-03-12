ddcf23979a0beca1a1de147b0405ec8083b0e3af
lenovo <1299088269@qq.com>
2019-03-12 08:24:16 +0800

    util module created

---
+++ util/src/com/java/util/hello.java
@@ -0,0 +1,11 @@
+package com.java.util;
+
+/**
+ * Created by lenovo on 2019/3/12 8:13
+ */
+public class hello {
+    public static void main (String[] args) {
+        System.out.println("hello");
+    }
+
+}
