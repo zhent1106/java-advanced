c3b1885364e0d0d2cd071c2aa9d03ab4f812638d
lenovo <1299088269@qq.com>
2019-04-18 00:12:35 +0800

    爬虫起步练习

---
+++ thread-study/src/com.soft1841.thread/jsoup/JsoupDemo.java
@@ -0,0 +1,24 @@
+package com.soft1841.thread.jsoup;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/**
+ * Created by 田震 on 2019/4/18 00:11
+ */
+public class JsoupDemo {
+    public static void main (String[] args) throws Exception{
+        String url="http://www.importnew.com/all-posts";
+        Connection connection= Jsoup.connect(url);
+        Document document=connection.get();
+        Elements elements=document.getElementsByClass("post-meta");
+        System.out.println(elements.size());
+        for (Element  element:elements
+             ) {
+            Element link=element.child(0).child(0);
+            String titleString=link.text();
+            System.out.println(titleString);
+        }
+    }
+}
